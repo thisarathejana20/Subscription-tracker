afa63afd5785363bae7faf7a71bff93b71bb46bd
thisaramax20 <thisarathejana20@gmail.com>
2025-02-10 19:53:15 +0530

    configure express

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+export default app;
